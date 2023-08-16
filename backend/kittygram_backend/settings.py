005dacdb28275633544752ebe288c129795ffcb6
Данил Данилыч <zolotoysnich@yandex.ru>
2023-08-16 09:38:31 +0300

    A nu ka

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,14 +4,12 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path('/backend/kittygram_backend/.env')
-load_dotenv(dotenv_path=env_path)
-
-# load_dotenv()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = True
 # DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
